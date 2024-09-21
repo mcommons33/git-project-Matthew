00f292697727cf6c7d86275db5007e4952c71362
Kyara Zhou <kzhou1@hwemail.com>
2024-09-20 19:41:11 -0700

    fixed tester for initalizeGitRepo

--- GitTester.java
+++ GitTester.java
@@ -5,12 +5,26 @@ public class GitTester {
     // compete wip
     // need to know why delete is not working
     public static void main(String[] args) {
+        deleteGitFolder();
+        Git.initalizeGitRepo();
 
-        System.out.println(Files.exists("./git/objects/index"));
-        // if (Files.exists("index")) {
-        //     folder.delete();
-        // }
-        //Git.initalizeGitRepo();
+    }
 
+    private static void deleteGitFolder() {
+        // deletes the git Repo
+        String path = "./git";
+        File toBeDelete = new File(path + "/objects");
+        deleteHelper(toBeDelete);
+        toBeDelete = new File(path + "/index");
+        deleteHelper(toBeDelete);
+        toBeDelete = new File(path);
+        deleteHelper(toBeDelete);
+    }
+
+    private static void deleteHelper(File toBeDelete) {
+        // if file exist, then delete
+        if (toBeDelete.exists()) {
+            toBeDelete.delete();
+        }
     }
 }
