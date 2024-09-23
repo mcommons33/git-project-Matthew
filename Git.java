9e0e8ee5ffd642b771604cbd126ff82845c33be1
Kyara Zhou <kzhou1@hwemail.com>
2024-09-22 19:43:40 -0700

    completed the SHA1 hasing thing

--- Git.java
+++ Git.java
@@ -1,15 +1,22 @@
-import java.io.IOException;
 import java.nio.file.*;
 import java.security.MessageDigest;
 import java.io.*;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
 
 public class Git {
    public static void main(String[] args) {
-      initalizeGitRepo();
+      try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("testingFile"))) {
+         bufferedWriter.write("look behind you");
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      File gitFolder = new File("./testingFile");
+      System.out.println(gitBlob(gitFolder));
    }
 
+   // initalizes a Git Repo with an "objects" folder and "index" file
    public static void initalizeGitRepo() {
-
       File gitFolder = new File("./git"); // git folder
       File objectsFolder = new File("./git/objects"); // objects folder
       File file = new File("./git", "index"); // index file
@@ -38,7 +45,35 @@ public class Git {
 
    }
 
-   // public static String gitBlob(File file) {
+   public static String gitBlob(File file) {
+      
+      // converts data in the file into a long string
+      StringBuilder sb = new StringBuilder();
+      try (BufferedReader bf = new BufferedReader(new FileReader(file));) {
+         while (bf.ready()) {
+            sb.append(bf.readLine());
+         }
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      String input = sb.toString();
+      //////
 
-   // }
+      // hashes the string
+      try {
+         MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+         crypt.update(input.getBytes("UTF-8"));
+         BigInteger temp = new BigInteger(1, crypt.digest());
+         return temp.toString(16);
+      } catch (NoSuchAlgorithmException e) {
+         e.printStackTrace();
+      } catch (UnsupportedEncodingException e) {
+         e.printStackTrace();
+      }
+
+
+      // this is here bc otherwise there is an error message. well can anyone actually bypass the "try"?
+      return "error"; 
+   }
+
 }
