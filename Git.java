a16c3bd480f8b51abaa4766577da60e6c27c72a6
Kyara Zhou <kzhou1@hwemail.com>
2024-09-24 12:10:57 -0700

    yes (#1)
    * rough draft for initalizeGitRepo
    
    added initalizeGitRepo + tester
    
    * completed initalizeGitRepo
    
    git tester is still a wip
    
    * fixing initalizeGitRepo
    
    * fixed tester for initalizeGitRepo
    
    * completed the SHA1 hasing thing
    
    * blob wip
    
    almost done except for having an extra line at the end + stretch goal
    
    * completed main + stretch1&2
    
    do I have time for stretch 3,,

--- Git.java
+++ Git.java
@@ -1,5 +1,110 @@
-public class Git{
- public static void main(String[] args) {
-    
- }
+import java.nio.file.*;
+import java.security.MessageDigest;
+import java.io.*;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Blob;
+
+public class Git {
+   // initalizes a Git Repo with an "objects" folder and "index" file
+   public static void initalizeGitRepo() {
+      File gitFolder = new File("./git"); // git folder
+      File objectsFolder = new File("./git/objects"); // objects folder
+      File file = new File("./git", "index"); // index file
+
+      if (objectsFolder.exists() && file.exists()) {
+         System.out.println("Git Repository already exists");
+      }
+
+      // creates the git folder
+      if (!gitFolder.exists()) {
+         gitFolder.mkdirs();
+      }
+
+      // creates the objects folder
+      if (!objectsFolder.exists()) {
+         objectsFolder.mkdirs();
+      }
+
+      // creates the file index
+      String filePath = file.getPath();
+      try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
+         bufferedWriter.write("");
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+   }
+
+   // hashes the content of a file using SHA1
+   public static String SHA1Hashing(File file) {
+      String content = fileContent(file); // getting the content
+      try {
+         MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+         crypt.update(content.getBytes("UTF-8"));
+         BigInteger temp = new BigInteger(1, crypt.digest());
+         return temp.toString(16);
+      } catch (NoSuchAlgorithmException e) {
+         e.printStackTrace();
+      } catch (UnsupportedEncodingException e) {
+         e.printStackTrace();
+      }
+
+      // this is here bc otherwise there is an error message. well can anyone actually
+      // bypass the "try"?
+      return "error";
+   }
+
+   // converts data in the file into a single string
+   private static String fileContent(File file) {
+      StringBuilder sb = new StringBuilder();
+      try (BufferedReader bf = new BufferedReader(new FileReader(file));) {
+         while (bf.ready()) {
+            sb.append(bf.readLine());
+            if (bf.ready()) {
+               // if has next line, then add a "\n"
+               sb.append("\n");
+            }
+         }
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return sb.toString();
+   }
+
+   public static void blobCreation(File file) {
+      String name = SHA1Hashing(file); // getting the hashed name
+      String content = fileContent(file); // getting the content
+
+      File blobFile = new File("./git/objects", name); // the blob file
+      // creates the blob file
+      String path = blobFile.getPath();
+      try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path))) {
+         bufferedWriter.write(content);
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+      // adds a new line into the index file
+      try {
+         FileWriter fw = new FileWriter("./git/index", true);
+         BufferedWriter bufferedWriter = new BufferedWriter(fw);
+         // writes the content into index
+         FileReader checks = new FileReader("./git/index");
+         if (!checks.ready()) {
+            bufferedWriter.write(name + " " + file.getName());
+         } else {
+            bufferedWriter.write("\n" + name + " " + file.getName());
+
+         }
+
+         // bufferedWriter.write(name + " " + file.getName() + "\n");
+         bufferedWriter.close();
+         checks.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+   }
+
 }
---
+++ GitTester.java
@@ -0,0 +1,72 @@
+import java.nio.file.*;
+import java.security.MessageDigest;
+import java.io.*;
+
+public class GitTester {
+    public static void main(String[] args) {
+        // testing file creation
+        deleteGitFolder();
+        Git.initalizeGitRepo();
+
+        // testing blob creation
+        File tester1 = fileCreation("tester1", "look behind you");
+        File tester2 = fileCreation("tester2", "hi\nthis is sonarii");
+        Git.blobCreation(tester1);
+        Git.blobCreation(tester2);
+        File tester3 = fileCreation("tester3", "hmmm");
+        Git.blobCreation(tester3);
+
+        // testing file deletion
+        deleteTesters();
+
+    }
+
+    // creates a file with given name and content
+    private static File fileCreation(String name, String content) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(name))) {
+            bufferedWriter.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new File("./" + name);
+    }
+
+    // deletes the git Repo (including index and objects)
+    private static void deleteGitFolder() {
+        String path = "./git";
+        File toBeDelete = new File(path + "/objects");
+        deleteHelper(toBeDelete);
+        toBeDelete = new File(path + "/index");
+        deleteHelper(toBeDelete);
+        toBeDelete = new File(path);
+        deleteHelper(toBeDelete);
+    }
+
+    // delete the 3 testers created (tester1, tester2, tester3) + its content in
+    // index and objects folder
+    private static void deleteTesters() {
+        File toBeDelete = new File("./tester1");
+        deleteHelper(toBeDelete);
+        toBeDelete = new File("./tester2");
+        deleteHelper(toBeDelete);
+        toBeDelete = new File("./tester3");
+        deleteHelper(toBeDelete);
+        toBeDelete = new File("./git/index");
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("./git/index"))) {
+            bufferedWriter.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File objectesFolder = new File("./git/objects");
+        for (File subfile : objectesFolder.listFiles()) {
+            subfile.delete();
+        }
+    }
+
+    private static void deleteHelper(File toBeDelete) {
+        // if file exist, then delete
+        if (toBeDelete.exists()) {
+            toBeDelete.delete();
+        }
+    }
+}
