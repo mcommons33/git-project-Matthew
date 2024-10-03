8b60ac6878092c99ba9c6e03cb8c9165ddab1059
Aviv Pilipski <apilipski1@hwemail.com>
2024-10-03 09:02:08 -0700

    Part 3 Complete

--- CommitTester.java
+++ CommitTester.java
@@ -5,6 +5,12 @@ public class CommitTester {
         Git myGit = new Git();
         myGit.initalizeGitRepo();
         myGit.stage("tester1");
-        myGit.stage("tester1");
+        myGit.stage("tester2");
+        System.out.println(myGit.commit("Aviv Pilipski", "Updated Tester1 and Tester2"));
+        myGit.stage("tester3");
+        System.out.println(myGit.commit("Aviv Pilipski", "Added file3"));
+        myGit.stage("testDir");
+        myGit.stage("testDirFiles");
+        System.out.println(myGit.commit("Aviv Pilipski", "Empty Directory + Directory with Files"));
     }
 }
