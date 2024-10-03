c53a6b996e817e3d6baa03ba97317a3894295654
Aviv Pilipski <apilipski1@hwemail.com>
2024-10-02 17:11:02 -0700

    Part 3: Commit Outline

---
+++ CommitTester.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class CommitTester {
+    public static void main (String [] args) throws IOException {
+        Git myGit = new Git();
+        myGit.initalizeGitRepo();
+        myGit.stage("tester1");
+        myGit.stage("tester2");
+        myGit.commit("Aviv Pilipski", "Updated Tester1 and Tester2");
+    }
+}
